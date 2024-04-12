98d370e04832706ea6c7c309a1b9cabe7e0f71b2
kadaji777 <roinwani@gmail.com>
2024-04-12 20:09:29 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
-}
+    // Select the input#button element
+    const input = document.getElementById('button');
+  
+    // Define a function to be executed when the event occurs
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add an event listener for the click event, passing the function as the callback
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to set up the event listener
+  addingEventListener();
+  
